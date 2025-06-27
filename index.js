7529d531e23b1a1f217517fd45ab4464c240794c
TURJA MONDAL <turjamondal1437@gmail.com>
2025-06-27 22:18:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+// console.log('HELLO WORLD');
+// var x; //undefined
+
+// function add () {
+//     var x; //hosting 
+//     x = 9;
+//     if (x<10) {
+//         console.log(x); // 9
+//         x = 20;
+//         console.log(x) // x=20
+//     }
+// }
+
+
+// function add () {
+//     let x; //hosting 
+//     x = 9;
+//     if (x<10) {
+//         console.log(x); // 9
+//         x = 20;
+//         console.log(x) // x=20
+//     }
+// }
+
+//number:
+let num = 20;
+
+//string:
+let nameText = 'Rishi';
+
+// boolean:
+let isStudent = true;
+
+//num:
+let text = null;
+
+//undefined:
+let a;
+
+//object;
+let person = { 
+    namePerson: 'Rishi', 
+    age: '30'
+}
+// cange acn possible  here
+person.age= 40;
+
+//array
+let arr = [10,20];
+let friends = ["Ram","Shyam","Balram"];
+console.log(friends.length); // 3
+console.log(friends[2]); // Balram
+friends.pop();
+console.log(friends.length); // 2
+
+friends.push("Krishna","Madhav");
+console.log(friends.length); // 4
+
+let nums = [1,2,6,8,4,9,8,4]
+
+let boolean = [ true, false, true];
+
+let persons = [
+    {name: 'a' , age: '20'},
+    {name: 'b' , age: '27'},
+];
+
+// mixed
+let mixed = [ 42, 'hello' , true, {name: x= 50}];
+
+//01:10:00 
