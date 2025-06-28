8ffeae863968dae3a79c9a72d046f032e6e27e15
TURJA MONDAL <turjamondal1437@gmail.com>
2025-06-28 21:26:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,98 +1,73 @@
-// // console.log('HELLO WORLD');
-// // var x; //undefined
+// Number
+let num = 20;
 
-// // function add () {
-// //     var x; //hosting 
-// //     x = 9;
-// //     if (x<10) {
-// //         console.log(x); // 9
-// //         x = 20;
-// //         console.log(x) // x=20
-// //     }
-// // }
+// String
+let nameText = 'Rishi';
 
+// Boolean
+let isStudent = true;
 
-// // function add () {
-// //     let x; //hosting 
-// //     x = 9;
-// //     if (x<10) {
-// //         console.log(x); // 9
-// //         x = 20;
-// //         console.log(x) // x=20
-// //     }
-// // }
+// Null
+let text = null;
 
-// //number:
-// let num = 20;
+// Undefined
+let a;
 
-// //string:
-// let nameText = 'Rishi';
+// Object
+let person = { 
+    namePerson: 'Rishi', 
+    age: '30'
+}
 
-// // boolean:
-// let isStudent = true;
+// Update object property
+person.age = 40;
 
-// //num:
-// let text = null;
+// Array of numbers
+let arr = [10, 20];
 
-// //undefined:
-// let a;
+// Array of strings
+let friends = ["Ram", "Shyam", "Balram"];
 
-// //object;
-// let person = { 
-//     namePerson: 'Rishi', 
-//     age: '30'
-// }
-// // cange acn possible  here
-// person.age= 40;
+console.log(friends.length); // Output: 3
+console.log(friends[2]);     // Output: Balram
 
-// //array
-// let arr = [10,20];
-// let friends = ["Ram","Shyam","Balram"];
-// console.log(friends.length); // 3
-// console.log(friends[2]); // Balram
-// friends.pop();
-// console.log(friends.length); // 2
+friends.pop();
+console.log(friends.length); // Output: 2
 
-// friends.push("Krishna","Madhav");
-// console.log(friends.length); // 4
+friends.push("Krishna", "Madhav");
+console.log(friends.length); // Output: 4
 
-// let nums = [1,2,6,8,4,9,8,4]
+// Array of numbers
+let nums = [1, 2, 6, 8, 4, 9, 8, 4];
 
-// let boolean = [ true, false, true];
+// Array of booleans
+let boolean = [true, false, true];
 
-// let persons = [
-//     {name: 'a' , age: '20'},
-//     {name: 'b' , age: '27'},
-// ];
+// Array of objects
+let persons = [
+    { name: 'a', age: '20' },
+    { name: 'b', age: '27' },
+];
 
-// // mixed
-// let mixed = [ 42, 'hello' , true, {name: x= 50}];
+// Mixed array (number, string, boolean, object)
+let mixed = [42, 'hello', true, { name: x = 50 }];
 
-// //01:10:00 Day two 
+// Program to print name, age, favourite colour
+let hisName = "Turja";
+let age = 23;
+let favouriteColour = "Black";
 
-// // comments
-// // single line comments
+console.log(hisName);
+console.log(age);
+console.log(favouriteColour);
 
-// /*
-// multiline comments
-// */
+// Using an object for the same data
+let person2 = {
+    namePerson: "Turja",
+    age: 23,
+    favouriteColour: "Black"
+}
 
-// /*
-// Write a program to print your name , age , and favourite colour using variables
-// */
-// let hisName = "Turja";
-// let age = 23;
-// let favouriteColour = "Black";
-// console.log(hisName)
-// console.log(age)
-// console.log(favouriteColour)
-
-// let person = {
-//      namePerson : "Turja",
-//      age : 23,
-//      favouriteColour : "Black"
-//  }
-// console.log(person.namePerson)
-// console.log(person.age)
-// console.log(person.favouriteColour)
-
+console.log(person2.namePerson);
+console.log(person2.age);
+console.log(person2.favouriteColour);
