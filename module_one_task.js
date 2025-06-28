16c3d328fc7a09e7375de2783213307a7e9e35f8
TURJA MONDAL <turjamondal1437@gmail.com>
2025-06-28 21:27:21 +0530

    Update module_one_task.js

--- module_one_task.js
+++ module_one_task.js
@@ -1,20 +1,21 @@
-/*
-Write a program to print your name , age , and favourite colour using variables
-*/
+// Program to print name, age, and favourite colour using variables
 let nameh = "Turja";
 let age = 23;
 let favouriteColour = "Black";
+
 console.log(`
     Name: ${nameh},
     Age: ${age},
-    Favourite Coulur  ${favouriteColour},
-    `)
+    Favourite Colour: ${favouriteColour}
+`);
 
+// Same data stored in an object
 let person = {
-     namePerson : "Turja",
-     age : 23,
-     favouriteColour : "Black"
- }
-console.log(person.namePerson)
-console.log(person.age)
-console.log(person.favouriteColour)
+    namePerson: "Turja",
+    age: 23,
+    favouriteColour: "Black"
+}
+
+console.log(person.namePerson);
+console.log(person.age);
+console.log(person.favouriteColour);
